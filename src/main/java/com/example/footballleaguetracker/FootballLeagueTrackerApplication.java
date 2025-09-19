fb6967e563774e9f8c4e224b677e6ab84474d3b9
Yorgo Haykal <yorgohaykal004@gmail.com>
2025-09-19 10:35:56 +0200

    Initial commit: Spring Boot project

---
+++ src/main/java/com/example/footballleaguetracker/FootballLeagueTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.footballleaguetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballLeagueTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballLeagueTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/footballleaguetracker/FootballLeagueTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.footballleaguetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballLeagueTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
